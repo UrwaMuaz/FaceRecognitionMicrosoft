e3d5a12a2adad42169b21ec338ebfaf58b2776dc
urwa <urwa.muaz@confiz.com>
2018-08-08 19:59:10 +0500

    Code Fixed

--- group.py
+++ group.py
@@ -2,16 +2,10 @@ import http.client, urllib.request, urllib.parse, urllib.error, base64, requests
 import cognitive_face as CF
 import requests
 from io import BytesIO
-from PIL import Image, ImageDraw
 import cv2
 import base64
 import os
 import time
-
-def is_male(attr):
-    if attr == 'male':
-        return True
-
 
 
 def recogn(KEY, img_url):
@@ -21,15 +15,14 @@ def recogn(KEY, img_url):
     detected = CF.face.detect(img_url)
     print (detected)
     return detected
-    
-   
+      
 
 imageFiles = os.listdir("images")
 faceIds = []
 images = []
 
 basedir = "images/"
-key = "c7cce2e49cb84d7bbe09a5b75535468a"
+key = "##########" # replace with your API key
 
 notdetected = 0
 for i,url in enumerate(imageFiles):
@@ -46,15 +39,12 @@ for i,url in enumerate(imageFiles):
     time.sleep(3)
 
 
-#print (faceIds)
-
 subscription_key = key
 uri_base = 'https://westcentralus.api.cognitive.microsoft.com'
 headers = {
     'Ocp-Apim-Subscription-Key': subscription_key,
 }
 
-#body = {'url': url}
 body = {'faceIds': faceIds}
 
 response = requests.request('POST', uri_base + '/face/v1.0/group', json=body, data=None, headers=headers, params=None)
@@ -62,6 +52,10 @@ parsed = json.loads(response.text)
 
 groups = parsed['groups']
 mess = parsed['messyGroup']
+
+
+if not os.path.exists("groups"):
+    os.makedirs("groups")
 
 for i,group in enumerate(groups):
     directory = "groups/"+str(i)
@@ -74,13 +68,12 @@ for i,group in enumerate(groups):
         imageToCopy = "images/"+str(images[ind])
         print("imageTocopy: "+str(imageToCopy))
         img = cv2.imread(imageToCopy,1)
-        cv2.imwrite(directory+"/"+images[ind], img)
-        
-    
+        cv2.imwrite(directory+"/"+images[ind], img)          
 
 directory = "groups/Mess"
 if not os.path.exists(directory):
     os.makedirs(directory)
+    
 for j,faceId in enumerate(mess):
     ind = faceIds.index(faceId)
 
@@ -89,7 +82,7 @@ for j,faceId in enumerate(mess):
     img = cv2.imread(imageToCopy,1)
     cv2.imwrite(directory+"/"+images[ind], img)
         
-print("not detected: "+str(notdetected))
-print(parsed)
+print("Number of Faces not detected: "+str(notdetected))
+#print(parsed)
 
 
